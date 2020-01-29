e3f542d4e292f945f950d8a2de29ee8b2f712bd9
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-01-29 12:04:06 +0100

    crud with postgresql

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,60 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    password: '',
+    database: 'firstapi',
+    port: '5432'
+});
+
+const getUsers = async (req, res) => {
+    const response = await pool.query('SELECT * FROM users');
+    res.status(200).json(response.rows);
+}
+
+const getUserById = async (req, res) => {
+    const id = req.params.id;
+    const response = await pool.query('SELECT * FROM users WHERE id= $1', [id]);
+    res.json(response.rows);
+}
+
+const createUser = async (req, res) => {
+    const { name, email } = req.body;
+    const response = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
+    console.log(response);
+    res.json({
+        message: 'User Added Succesfully',
+        body: {
+            user: {name, email}
+        }
+    });
+}
+
+const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const { name, email } = req.body;
+    console.log(req.body);
+    const response = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [
+        name, 
+        email, 
+        id
+    ]);
+    console.log(response);
+    res.send('User Updated succesfully');
+}
+
+const deleteUser = async (req, res) => {
+    const id = req.params.id;
+    const response = await pool.query('DELETE FROM users WHERE id = $1', [id]);
+    console.log(response);
+    res.json(`User ${id} deleted succesfully`);
+}
+
+module.exports = {
+    getUsers, 
+    createUser,
+    getUserById,
+    deleteUser,
+    updateUser
+}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ const express = require('express');
 const port = process.env.PORT || 9000;
 const app = express();
 
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// routes
+app.use(require('./routes/index'));
+
 app.listen(port);
 console.log(`Server listening on port ${port}`);
